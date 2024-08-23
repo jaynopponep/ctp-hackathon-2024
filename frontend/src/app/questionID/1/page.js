739313e31b180defc646aaa9c19220d70b179bb7
Yared Pena <102680810+YaredPena@users.noreply.github.com>
2024-08-23 15:54:47 -0400

    questions upload with routing (no quest params)

---
+++ frontend/src/app/questionID/1/page.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import QuestionPage from '../../components/QuestionPage'; // For relative import
+
+const Question = () => {
+  return <QuestionPage />;
+};
+
+export default Question;
